fa6218f13b0f646fb2cbe66e74fd45e59fad25f0
deepak-raaaz <deepakjamui26@gmail.com>
2025-05-28 01:17:21 +0530

    added server app

---
+++ server/server.ts
@@ -0,0 +1,9 @@
+import { app } from "./app";
+require("dotenv").config();
+import connectDB from "./utlis/db";
+
+// create server
+app.listen(8080, () => {
+  console.log(`server is connected with port ${process.env.PORT}`);
+  connectDB();
+});
