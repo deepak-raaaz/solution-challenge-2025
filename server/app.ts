a2cd6c31886ec2cb5f66230071958f311298d2a6
deepak-raaaz <deepakjamui26@gmail.com>
2025-06-12 15:31:17 +0530

    cors origin changed

--- server/app.ts
+++ server/app.ts
@@ -20,7 +20,7 @@ app.use(cookieParser());
 // cors = cross origin resource sharing
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://student-ui-554347060569.asia-southeast1.run.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
