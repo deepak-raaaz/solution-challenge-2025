1d124f18d7f27f1fe9b511a689b60b9283ccb9ef
deepak-raaaz <deepakjamui26@gmail.com>
2025-06-12 14:53:17 +0530

    docker 04

--- server/server.ts
+++ server/server.ts
@@ -4,7 +4,7 @@ require("dotenv").config();
 import connectDB from "./utlis/db";
 
 // create server
-app.listen(8080, () => {
+app.listen(8080,'0.0.0.0', () => {
   console.log(`server is connected with port ${process.env.PORT}`);
   connectDB();
 });
