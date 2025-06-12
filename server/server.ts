9ee631a524c637735c0ac4e84287a7234d6d9038
deepak-raaaz <deepakjamui26@gmail.com>
2025-06-12 13:00:44 +0530

    port error correction 1

--- server/server.ts
+++ server/server.ts
@@ -2,9 +2,10 @@ import { app } from "./app";
 require("dotenv").config();
 import connectDB from "./utlis/db";
 
+const port = process.env.PORT || 8080;
 
 // create server
-app.listen(8082, () => {
-  console.log(`server is connected with port ${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`server is connected with port ${port}`);
   connectDB();
 });
